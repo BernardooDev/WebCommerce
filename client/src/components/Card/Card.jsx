edceeddd54e3c796cd68a77d2521f99491b3d9e6
BernardooDev <bernardo.almeida@uscsonline.com.br>
2023-07-18 23:11:30 -0300

    feat: using the reduce for making the sum of the product adding to the cart

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,30 +1,36 @@
-import { useContext } from "react"
-// import { CardPage } from "./Style"
-import { CartContext } from "../../context/Cart"
+import { useContext } from "react";
+import { CartItems, CartPage, SingleItem } from "./Style";
+import { CartContext } from "../../context/Cart";
 import axios from "axios";
 import { useQuery } from "react-query";
 
 function Card() {
   const { cartItems } = useContext(CartContext);
+
   const { data } = useQuery("sneaker", async () => {
     const response = await axios.get("http://localhost:3003/sneaker");
     return response.data;
   });
 
-  const filteredItems = data?.filter((item) => cartItems.includes(item.id));
+  const Sneakers = data?.filter((item) => cartItems.includes(item.id));
+
+  const PriceOfSneakers = Sneakers?.map((d) => Number(d.price));
+
+  const SumOfSneaker = PriceOfSneakers?.reduce((a, b) => a + b);
 
   return (
-    <div>
-      {filteredItems &&
-        filteredItems.map((item) => (
-          <div key={item.id}>
+    <CartPage>
+      <CartItems>
+        {Sneakers?.map((item, id) => (
+          <SingleItem key={id}>
             <h1>{item.product}</h1>
             <p>{item.price}</p>
-          </div>
+          </SingleItem>
         ))}
-    </div>
+        <h3>Total: {SumOfSneaker}</h3>
+      </CartItems>
+    </CartPage>
   );
 }
 
 export default Card;
-
--- client/src/components/Card/Style.js
+++ client/src/components/Card/Style.js
@@ -1,6 +1,28 @@
 import styled from "styled-components"
 
-export const CardPage = styled.main`
+export const CartPage = styled.main`
     width: 100vw;
     height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+export const CartItems = styled.section`
+    width: 60%;
+    height: 80%;
+    border: 1px solid;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+export const SingleItem = styled.div`
+    width: 100%;
+    height: 50%;
+    border: 1px solid;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
 `
